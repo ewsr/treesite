80b8e531cd5a53714dbee902ee0e332b6267abe1
ewsr <emily67948@gmail.com>
2019-08-11 22:09:37 -0400

    pages

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,3 +1,24 @@
 class Species < ApplicationRecord
     has_many :trees
-end
+
+    def get_latin_name
+        name_array = []
+        name_array += genus.present? ? [genus] : []
+        name_array += species.present? ? [species] : []
+        name_array += cultivar.present? ? ["'"+cultivar+"'"] : []
+        string = name_array.reject(&:empty?).join(" ")
+        return string.present? ? string : "Tree #{id}"
+    end 
+
+    def get_common_name
+        common_name
+    end 
+
+    def get_description
+        description
+    end 
+end 
+
+    
+
+
